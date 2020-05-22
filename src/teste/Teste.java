ebee6f2239ce04ae019a90b44d75c877df6eaa37
João Carlos Carvalho Leite <joao-leite8@hotmail.com>
2020-05-22 18:22:56 +0100

    Revert "safasf"
    This reverts commit 397a6a91f73e9bac2dc835cbd85648f9a9921e6e.

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -15,7 +15,7 @@ public class Teste {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Ola");
+        // TODO code application logic here
     }
     
 }
