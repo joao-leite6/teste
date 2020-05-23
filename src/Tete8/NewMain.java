2d1fb1142acb56430f460d6eeb81e22ce4944aa1
João Carlos Carvalho Leite <joao-leite8@hotmail.com>
2020-05-23 15:08:47 +0100

    teste n 69

--- src/Tete8/NewMain.java
+++ src/Tete8/NewMain.java
@@ -15,7 +15,7 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("vai shdhas");
+        int flar = 0;
     }
     
 }
