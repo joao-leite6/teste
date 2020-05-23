f41234e1e2f096ad81f8775c2902bcdafbb0c54a
Célio Macedo <8170358@estg.ipp.pt>
2020-05-23 15:15:54 +0100

    alterei o main

--- src/Tete8/NewMain.java
+++ src/Tete8/NewMain.java
@@ -15,13 +15,14 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("vai shdhas");
         System.out.println("vai rawrteuywuyqruyreryuwquryeurywquyrteytuqyuwey");
         System.out.println("vai asjkdjksajdjk");
-=======
+
         int flar = 0;
->>>>>>> master
+         System.out.println(flar);
+        
     }
     
 }
